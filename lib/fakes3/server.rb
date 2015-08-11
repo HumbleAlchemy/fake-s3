c7a5e5868c6e8f924ac727d54787d45e9844184a
HumbleAlchemy <abhishek.alchemist@gmail.com>
2015-08-11 15:31:22 +0530

    changed wildcards to explicit method specification
    Added all the methods 'POST, PUT, DELETE, GET, OPTIONS' in add_cors_headers method

--- lib/fakes3/server.rb
+++ lib/fakes3/server.rb
@@ -222,7 +222,7 @@ module FakeS3
     private
     def add_cors_headers(request, response)
       response['Access-Control-Allow-Origin'] = '*' if request['Origin']
-      response['Access-Control-Allow-Methods'] = '*' if request['Origin']
+      response['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS' if request['Origin']
     end
 
     def get_bucket(bucket)
